fbd330bfac7cf6d5a12b0083922849993e6560c1
Dan Manning <dmanning@oceaneering.com>
2020-02-27 17:23:47 -0600

    nugets

--- SQLiteConnectionBuddyTest.Android/Resources/Resource.Designer.cs
+++ SQLiteConnectionBuddyTest.Android/Resources/Resource.Designer.cs
@@ -46,11 +46,11 @@ namespace SQLiteConnectionBuddyTest.Android
 		public partial class Drawable
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			// aapt resource value: 0x7F010000
+			public const int Icon = 2130771968;
 			
-			// aapt resource value: 0x7f020001
-			public const int Splash = 2130837505;
+			// aapt resource value: 0x7F010001
+			public const int Splash = 2130771969;
 			
 			static Drawable()
 			{
@@ -65,11 +65,11 @@ namespace SQLiteConnectionBuddyTest.Android
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030001
-			public const int ApplicationName = 2130903041;
+			// aapt resource value: 0x7F020000
+			public const int ApplicationName = 2130837504;
 			
-			// aapt resource value: 0x7f030000
-			public const int Hello = 2130903040;
+			// aapt resource value: 0x7F020001
+			public const int Hello = 2130837505;
 			
 			static String()
 			{
@@ -84,8 +84,8 @@ namespace SQLiteConnectionBuddyTest.Android
 		public partial class Style
 		{
 			
-			// aapt resource value: 0x7f040000
-			public const int Theme_Splash = 2130968576;
+			// aapt resource value: 0x7F030000
+			public const int Theme_Splash = 2130903040;
 			
 			static Style()
 			{
